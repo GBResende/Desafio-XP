72e4239b56d8e7a69a1b9f39ad065a2fc568d785
Gabriel Resende <gabresendemkt@gmail.com>
2022-07-18 20:43:46 -0300

    feat(routers): :art: create router users & stocks

--- api/server.js
+++ api/server.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/users', require('./routers/users'));
+app.use('/stocks', require('./routers/stocks'));
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
