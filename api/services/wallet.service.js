6d39a941abec13299326495acab97da431dfd447
Gabriel Resende <gabresendemkt@gmail.com>
2022-07-20 23:23:08 -0300

    refactor(service): :art: change destructuring of wallet service

--- api/services/wallet.service.js
+++ api/services/wallet.service.js
@@ -1,14 +1,18 @@
+const { errorObj } = require('../middlewares/error.middleware');
 const walletModel = require('../models/wallet.model');
 
 const getUserStockById = async (payload) => {
-  const { userId, stockId } = payload;
-  const userStock = await walletModel.getUserStockById(userId, stockId);
+  const { id, stockId } = payload;
+  const userStock = await walletModel.getUserStockById(id, stockId);
+  if (!userStock) {
+    throw errorObj(400, 'O usuário não possui essa ação');
+  }
   return userStock;
 };
 
 const getUserWallet = async (payload) => {
-  const { userId } = payload;
-  const userWallet = await walletModel.getUserWallet(userId);
+  const { id } = payload;
+  const userWallet = await walletModel.getUserWallet(id);
   return userWallet;
 };
 
