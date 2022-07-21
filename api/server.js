ec3918664dda36db82fb6bcd5d652eafdc673bb1
Gabriel Resende <gabresendemkt@gmail.com>
2022-07-20 23:33:58 -0300

    feat(routers): :art: implement login router on server and initialize config of swagger on server

--- api/server.js
+++ api/server.js
@@ -1,4 +1,8 @@
+require('express-async-errors');
+const swagger = require('swagger-ui-express');
+
 const express = require('express');
+const { errorMiddleware } = require('./middlewares/error.middleware');
 require('dotenv').config();
 
 const app = express();
@@ -8,7 +12,12 @@ app.use(express.json());
 app.use('/users', require('./routers/users'));
 app.use('/stocks', require('./routers/stocks'));
 app.use('/wallet', require('./routers/wallet'));
+app.use('/login', require('./routers/login'));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.use('/docs', swagger.serve, swagger.setup(require('../swagger.json')));
+
+app.use(errorMiddleware);
+
+app.listen(process.env.MYSQL_PORT, () => {
+  console.log(`Server is running on port ${process.env.MYSQL_PORT}`);
 });
