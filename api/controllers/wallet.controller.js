55238b2b4697a31ed69f1cc6eec9deb347fb5a7e
Gabriel Resende <gabresendemkt@gmail.com>
2022-07-20 23:31:41 -0300

    feat(controller): :art: implement middlewares on wallet controller

--- api/controllers/wallet.controller.js
+++ api/controllers/wallet.controller.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const { validateRequest } = require('../middlewares/request.middleware');
 
 const walletService = require('../services/wallet.service');
 
 const walletRouter = express.Router();
 
-walletRouter.get('/ativos/:id', async (req, res) => {
+walletRouter.get('/ativos/:id', validateRequest, async (req, res) => {
   const wallet = await walletService.getUserWallet(req.params);
   res.status(200).json(wallet);
 });
 
-walletRouter.get('/ativos/:id/stock/:stockId', async (req, res) => {
+walletRouter.get('/ativos/:id/stock/:stockId', validateRequest, async (req, res) => {
   const wallet = await walletService.getUserStockById(req.params);
   res.status(200).json(wallet);
 });
