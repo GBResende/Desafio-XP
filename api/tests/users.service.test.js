80721dcec42d6c53bf3cdeb060abf4d4f2fe27df
Gabriel Resende <gabresendemkt@gmail.com>
2022-07-24 14:35:05 -0300

    feat(tests): :art: create tests of users service getUserById, getUserAccount method

--- api/tests/users.service.test.js
+++ api/tests/users.service.test.js
@@ -28,3 +28,49 @@ describe('testa a camada de users Service', () => {
       expect(user).to.be.eql(usersMock.userByCredentials[0][0]);
     });
   });
+  describe('testa a o retorno de erro getUserById', () => {
+    beforeEach(() => {
+      sinon.stub(usersModel, 'getUserById').resolves();
+    });
+    afterEach(() => {
+      usersModel.getUserById.restore();
+    });
+    it('Deve retornar um usuário', async () => {
+      try {
+        await userService.getUserById({ id: 1 });
+      }
+      catch (err) {
+        expect(err.status).to.be.equal(404);
+        expect(err.message).to.be.equal('Usuário não encontrado');
+      }
+    });
+  })
+  describe('testa a função getUserAccount', () => {
+    beforeEach(async () => {
+      sinon.stub(usersModel, 'getUserAccount').resolves(usersMock.userAccount[0][0]);
+    });
+    afterEach(async () => {
+      usersModel.getUserAccount.restore();
+    });
+    it('Deve retornar um os dados do usuário incluindo balance', async () => {
+      const user = await userService.getUserAccount({ id: 1 });
+      expect(user).to.be.eql(usersMock.userAccount[0][0]);
+    });
+  });
+  describe('testa o retorno de erro getUserAccount', () => {
+    beforeEach(() => {
+      sinon.stub(usersModel, 'getUserAccount').resolves();
+    });
+    afterEach(() => {
+      usersModel.getUserAccount.restore();
+    });
+    it('Deve retornar um erro 404 com a mensagem "usuário não encontrado"', async () => {
+      try {
+        await userService.getUserAccount({ id: 1 });
+      }
+      catch (err) {
+        expect(err.status).to.be.equal(404);
+        expect(err.message).to.be.equal('Usuário não encontrado');
+      }
+    });
+  });
