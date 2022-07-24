a224194932c51a7c4bddb0c487a71a672bbe2ffd
Gabriel Resende <gabresendemkt@gmail.com>
2022-07-24 14:20:17 -0300

    feat(tests): :art: create stocks mock

---
+++ api/tests/mocks/stocks.mock.js
@@ -0,0 +1,23 @@
+const stocks = [
+  {
+    id: 1,
+    stock: 'Apple',
+    ticker: 'AAPL',
+    quantity: 10,
+    value: 211.00,
+  },
+  {
+    id: 2,
+    stock: 'Google',
+    ticker: 'GOOG',
+    quantity: 10,
+    value: 511.00,
+  },
+];
+
+const stockById = stocks[0];
+
+module.exports = {
+  stocks,
+  stockById,
+};
