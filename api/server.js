cbd80f1a6c42750d9009a4aa5d46ed859274ea2b
Gabriel Resende <gabresendemkt@gmail.com>
2022-07-24 15:54:09 -0300

    feat(config): :art: change port

--- api/server.js
+++ api/server.js
@@ -18,6 +18,6 @@ app.use('/docs', swagger.serve, swagger.setup(require('../swagger')));
 
 app.use(errorMiddleware);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
