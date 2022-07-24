4327a2b3d3741163ddd0373ea020fcc8d0071b99
Gabriel Resende <gabresendemkt@gmail.com>
2022-07-24 14:11:49 -0300

    feat(tests): :art: create wallet Mocks

---
+++ api/tests/mocks/wallet.mock.js
@@ -0,0 +1,32 @@
+const walletById = [
+  {
+    id: 1,
+    user_id: 1,
+    stock_id: 2,
+    quantity: 5,
+    updatedAt: '2022-07-22T16:26:11.000Z',
+    value: '322.00',
+  },
+  {
+    id: 2,
+    user_id: 1,
+    stock_id: 10,
+    quantity: 5,
+    updatedAt: '2022-07-22T16:26:20.000Z',
+    value: '241.00',
+  },
+];
+
+const walletByStockId = {
+  id: 1,
+  user_id: 1,
+  stock_id: 2,
+  quantity: 5,
+  updatedAt: '2022-07-22T16:26:11.000Z',
+  value: '322.00',
+};
+
+module.exports = {
+  walletById,
+  walletByStockId,
+};
